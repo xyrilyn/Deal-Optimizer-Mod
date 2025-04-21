a8a45eff63220949d699b0bd62677bc6f93b8cc5
xyrilyn <xyrilyn@outlook.com>
2025-04-21 14:33:36 +0800

    add new config options

--- src/IL2CPP/ModConfiguration.cs
+++ src/IL2CPP/ModConfiguration.cs
@@ -5,14 +5,18 @@ namespace DealOptimizer_IL2CPP
     public class ModConfiguration
     {
         // 01_Counteroffer
-        public static MelonPreferences_Entry<bool> CounterofferOptimizationEnabled;
+        public static MelonPreferences_Entry<bool> CounterofferUIEnabled;
         public static MelonPreferences_Entry<bool> PricePerUnitDisplay;
         public static MelonPreferences_Entry<bool> MaximumDailySpendDisplay;
 
-        // 02_Street Deals
+        // 02_Counteroffer_Optimization
+        public static MelonPreferences_Entry<bool> CounterofferOptimizationEnabled;
+        public static MelonPreferences_Entry<int> MinimumSuccessProbability;
+
+        // 03_Street Deals
         public static MelonPreferences_Entry<bool> StreetDealOptimizationEnabled;
 
-        // 03_Product_Evaluator
+        // 04_Product_Evaluator
         public static MelonPreferences_Entry<bool> ProductEvaluatorEnabled;
 
         // 09_Debug
@@ -21,14 +25,18 @@ namespace DealOptimizer_IL2CPP
         public static void SetupConfiguration()
         {
             var categoryCounteroffer = MelonPreferences.CreateCategory("DealOptimizer_IL2CPP_01_Counteroffer", "Counteroffer Settings");
-            CounterofferOptimizationEnabled = categoryCounteroffer.CreateEntry("CounterofferOptimizationEnabled", true, "Enable optimization for Counteroffers");
+            CounterofferUIEnabled = categoryCounteroffer.CreateEntry("CounterofferUI", true, "Enable Counteroffer UI");
             PricePerUnitDisplay = categoryCounteroffer.CreateEntry("PricePerUnitDisplay", true, "Display price per unit in UI");
             MaximumDailySpendDisplay = categoryCounteroffer.CreateEntry("MaximumDailySpendDisplay", true, "Display customer's max daily spend in UI");
 
-            var categoryStreetDeals = MelonPreferences.CreateCategory("DealOptimizer_IL2CPP_02_Street_Deals", "Street Deals Settings");
+            var categoryCounterofferOptimization = MelonPreferences.CreateCategory("DealOptimizer_IL2CPP_02_Counteroffer_Optimization", "Counteroffer Optimization Settings");
+            CounterofferOptimizationEnabled = categoryCounterofferOptimization.CreateEntry("CounterofferOptimizationEnabled", true, "Enable optimization for Counteroffers");
+            MinimumSuccessProbability = categoryCounterofferOptimization.CreateEntry("MinimumSuccessProbability", 98, "Min. success % for optimization");
+            
+            var categoryStreetDeals = MelonPreferences.CreateCategory("DealOptimizer_IL2CPP_03_Street_Deals", "Street Deals Settings");
             StreetDealOptimizationEnabled = categoryStreetDeals.CreateEntry("StreetDealOptimizationEnabled", true, "Enable optimization for Street Deals");
 
-            var categoryProductEvaluator = MelonPreferences.CreateCategory("DealOptimizer_IL2CPP_03_Product_Evaluator", "Product Evaluator Settings");
+            var categoryProductEvaluator = MelonPreferences.CreateCategory("DealOptimizer_IL2CPP_04_Product_Evaluator", "Product Evaluator Settings");
             ProductEvaluatorEnabled = categoryProductEvaluator.CreateEntry("ProductEvaluatorEnabled", false, "Enable Product Evaluator feature");
 
             var categoryDebug = MelonPreferences.CreateCategory("DealOptimizer_IL2CPP_09_Debug", "Debug Settings (May Cause Lag)");
